a1eebe8b70e6662a5f064924491d4c2eaccf3f50
Charlie <charjhall777@gmail.com>
2021-04-19 10:36:03 -0400

    adding the create log object function

--- server.js
+++ server.js
@@ -4,16 +4,35 @@ require('dotenv').config({
 const {
 	google
 } = require('googleapis');
-const { GoogleSpreadsheet } = require("google-spreadsheet");
+const {
+	GoogleSpreadsheet
+} = require("google-spreadsheet");
 const docs = require('@googleapis/docs');
 const express = require('express');
 const mysql = require('mysql2');
 const app = express();
 
+const { edit_dist } = require("./utils.js");
+// INTERNAL REPRASENTATION OF DAILY = 0, WEEKLY = 1, MONTHLY = 2, SEASONAL = 3, ANNUAL = 4, ONINCIDENT = 5, 
+// ASNEEDED = 6, CORRECTIVEACTION = 7, RISKASSESMENT = 8, PREHARVEST = 9, DELIVERYDAYS = 10
+const frequency_ofSubmission = {
+	daily: 0,
+	weekly: 1,
+	monthly: 2,
+	seasonal: 3,
+	annual: 4,
+	onincident: 5,
+	asneeded: 6,
+	correctiveaction: 7,
+	riskassesment: 8,
+	preharvest: 9,
+	deliverydays: 10
+}
+
 const SHEETS_SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
 
-const auth = new google.auth.GoogleAuth({
-	process.env.CLIENT_EMAIL, process.env.PRIVATE_KEY, SHEETS_SCOPES});
+// const auth = new google.auth.GoogleAuth({
+// 	process.env.CLIENT_EMAIL, process.env.PRIVATE_KEY, SHEETS_SCOPES});
 
 //To create user on table (after making database):
 //CREATE USER 'foodhubuser'@'localhost' IDENTIFIED BY '0f28901b-2644-4109-ab06-21f55d49438f';
@@ -66,10 +85,43 @@ async function create_main_log_object(sheet_id) {
 
 	await doc.loadInfo();
 	// pull all of the data from the spreadsheet to then parse through and make a return object
-	let full_row_data = doc.sheetsByIndex[0].getRows();
+	let full_row_data = await doc.sheetsByIndex[0].getRows();
+
+	// create an array that can store object data for each value
 	let all_sheet_logs = [];
+	full_row_data.forEach((log_row) => {
+		//console.log("\n\n", log_row._rawData);
+		// run through all the log data
+		// find the ones that have at least 3 items (meaning they have enough data to qualify)
+		if (log_row._rawData[0] && log_row._rawData[0].length && log_row._rawData[2] && log_row._rawData[2].length) {
+			log_row._rawData[2] = log_row._rawData[2].toLowerCase().replace(/[^a-z]/g, "");
+			// find what log_row._rawData[2] (the frequency of submission) is closest to (fuzzy wuzzy it)
+
+			let temporary_item;
+			let lowest_fuzzy = 10000;
+			Object.keys(frequency_ofSubmission).forEach((item) => {
+				// compare item and see which we will choose
+				let distance = edit_dist(log_row._rawData[2], item);
+				if (lowest_fuzzy > distance) {
+					temporary_item = item;
+					lowest_fuzzy = distance;
+				}
+			});
+			log_row._rawData[2] = frequency_ofSubmission[temporary_item];
+
+			// otherwise we need to encapsulate the important data into an object
+			all_sheet_logs.push({
+				file_name: log_row._rawData[0],
+				frequency_ofSubmission: log_row._rawData[2]
+			});
+		}
+	});
 	return all_sheet_logs;
 }
+
+create_main_log_object("1eao1E4mzLu4oXINDrezLIf8pOQh6L4J9LdUAgfOpYaw").then((sheet_answer) => {
+	console.log(sheet_answer);
+});
 
 app.get("/", (req, res) => {
 	res.end("Dumby server!");
---
+++ utils.js
@@ -0,0 +1,50 @@
+function make_array(w1, w2) {
+	let array = [];
+	let columns_value = w1.length > w2.length ? w1.length : w2.length;
+	let rows_value = w1.length > w2.length ? w2.length : w1.length;
+	for (let y = 0; y < columns_value + 1; y++) { // build the columns based on w1 value
+		array[y] = [];
+		for (let x = 0; x < rows_value + 1; x++) { // build the rows based on the w2 value
+			array[0][x] = x;
+			array[y][0] = y;
+			array[y][x] = 0;
+		}
+	}
+	return array;
+}
+
+function printMultiArray(arr) {
+	for (let y = 0; y < arr[0].length; y++) {
+		for (let x = 0; x < arr.length; x++) {
+			process.stdout.write(arr[x][y] + "\t");
+		}
+		process.stdout.write("\n");
+	}
+}
+
+function min(values) {
+	let min = 100000;
+	for (value in values) {
+		min = values[value] < min ? values[value] : min;
+	}
+	return min
+}
+
+function edit_dist(w1, w2) {
+	let array = make_array(w1, w2);
+	w1 = " " + w1;
+	w2 = " " + w2;
+	for (let y = 1; y < array.length; y++) { // go through the rows
+		for (let x = 1; x < array[0].length; x++) { // go through that full column
+			let sub_add = w1[y] == w2[x] ? 0 : 1; // check for adding onto sub case
+			// check the transpose case
+			array[y][x] = (w1[x] == w2[y - 1] && w1[x - 1] == w2[y]) ? array[y][x] = min([sub_add + array[y - 1][x - 1], 1 + array[y][x - 1], 1 + array[y - 1][x], 1 + array[y - 2][x - 2]]) :
+			array[y][x] = min([sub_add + array[y - 1][x - 1], 1 + array[y][x - 1], 1 + array[y - 1][x]]);
+		}
+	}
+	return array[array.length - 1][array[0].length - 1];
+}
+
+console.log(edit_dist("cat", "bat"));
+
+module.exports = { edit_dist };
