1d7d932df584b3a3e03c3940055c873889327934
Isabella Frank <isabella@isabellas-mbp.stab.org>
2021-05-03 10:27:01 -0400

    delete extra

--- admin.js
+++ admin.js
@@ -3,29 +3,13 @@ const bcrypt = require('bcrypt');
 
 const { connection, isLoggedIn } = require("./utils");
 
-
 const saltRounds = 10;
-const myPlaintextPassword = 'foodhub';
-const someOtherPlaintextPassword = 'foody';
-
-bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
-	//console.log(hash);
-    // Store hash in password DB.
-
-// get hash from database password DB.
-bcrypt.compare(myPlaintextPassword, hash, function(err, result) {
-    // result == true
-bcrypt.compare(someOtherPlaintextPassword, hash, function(err, result) {
-    // result == false
-});
-});
-});
 
 const router = express.Router()
 
-router.get("/make-farm", isLoggedIn, (req, res) => {
-	let test = {farm_name: "test", email: "tastfarm@gmail.com", password: "testpassword", root_folder: "testfarmfolder"}
-	bcrypt.hash(test.password, saltRounds, function(err, hash) {
+router.post("/make-farm", isLoggedIn, (req, res) => {
+	let test = {farm_name: req.body.farmname, email: req.body.username, password: req.body.psw, root_folder: req.body["root-folder"]}
+	bcrypt.hash(req.body.psw, saltRounds, function(err, hash) {
 		test.password = hash;
 		connection.query("INSERT INTO farmers (farm_name, email, password, root_folder) VALUES (?, ?, ?, ?) ON DUPLICATE KEY UPDATE farm_name=VALUES(farm_name), password=VALUES(password), root_folder=VALUE(root_folder)", Object.values(test), function (err) {
 			if (err) console.log(err);
@@ -35,14 +19,14 @@ router.get("/make-farm", isLoggedIn, (req, res) => {
 	});
 });
 
-router.get("/view-farms", isLoggedIn, (req, res) => {
+router.post("/view-farms", isLoggedIn, (req, res) => {
 	connection.query("SELECT * FROM farmers", function(err, farmers){
 		if (err) console.log(err);
 		
 	});
 });
 
-router.get("/delete-farm", isLoggedIn, (req,res) => {
+router.post("/delete-farm", isLoggedIn, (req,res) => {
 	connection.query("DELETE FROM farmers WHERE email = 'tastfarm@gmail.com' ", function(err,farmers){
 		if (err) console.log(err);
 		console.log("yay");
